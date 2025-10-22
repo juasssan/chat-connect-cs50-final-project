30bff6304faafef01274f98dca363b275684afe2
juasssan <ju.lioopro95@gmail.com>
2025-10-22 19:08:55 +0100

    chore: create iOS project

---
+++ ChatConnect/ChatConnect/ChatConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatConnectApp.swift
+//  ChatConnect
+//
+//  Created by Julio Cesar on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
